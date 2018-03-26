9546a1655cc2f87c9ac2df4e309dcf2498ac1208
ohsnaplol <emyb@bellsouth.net>
2018-03-25 22:58:15 -0400

    nickname and social network no longer an array, added friends

--- models/user.js
+++ models/user.js
@@ -3,16 +3,15 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   realName: { type: String, required: true },
-  nickNamesArray: [{
-    type: String
-  }],
+  nicknames: { type: String },
   email: { type: String, required: true },
   password: { type: String, required: true},
-  socialNetworkArray: [{
+  socialNetwork: {
     networkName: { type: String, required: true },
     userName: { type: String },
     url: { type: String }
-  }],
+  },
+  friends: { type: String },
   lastSettingsUpdate: { type: Date },
   dateJoined: { type: Date, default: Date.now }
 })
