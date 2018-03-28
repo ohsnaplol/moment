95907d02879bc35eef5f9add3f1db644c0afec81
ohsnaplol <emyb@bellsouth.net>
2018-03-27 21:16:26 -0400

    added router switch to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
