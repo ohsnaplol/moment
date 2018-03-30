e0b30fcc844621d3ae9fb0588afcd03ee08d5541
ohsnaplol <emyb@bellsouth.net>
2018-03-30 12:21:56 -0400

    corrected form layout, added states, handle input change, form submit

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -2,23 +2,62 @@ import React, { Component } from "react";
 import API from "../../utils/API";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password1: "",
+    password2: ""
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
 
   handleFormSubmit = event => {
     event.preventDefault()
     console.log(event)
+    // verify both passwords are the same
+    if (this.state.password1 === this.state.password2) {
+      let password = this.state.password1
+      let email = this.state.email
+      // check if email doesnt already exist in db
+    }
   }
 
   render() {
     return (
       <div>
-        <form>
-          <label>Email</label>
-          <input type="email"></input>
-          <label>Password</label>
-          <input type="password"></input>
-          <label>Verify Password</label>
-          <input type="password"></input>
-          <button onClick={this.handleFormSubmit}>Create Account</button>
+        <form onSubmit={this.handleFormSubmit}>
+          <label>
+            Email:
+            <input 
+              type="email" 
+              value={this.state.email} 
+              name="email"
+              onChange={this.handleInputChange}
+            ></input>
+          </label>
+          <label>
+            Password:
+            <input 
+              type="password" 
+              value={this.state.password1} 
+              name="password1"
+              onChange={this.handleInputChange}
+            ></input>
+          </label>
+          <label>
+            Verify Password:
+            <input 
+              type="password" 
+              value={this.state.password2} 
+              name="password2"
+              onChange={this.handleInputChange}
+            ></input>
+          </label>
+          <input type="submit" value="Create Account" />
         </form>
       </div>
     )
