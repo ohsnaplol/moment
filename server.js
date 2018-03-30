6c0518f2f0d1dd761d48fb2ddad4722bf662ac6a
ohsnaplol <emyb@bellsouth.net>
2018-03-29 20:10:56 -0400

    server now uses APIs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-// const routes = require("./routes")
+const routes = require("./routes")
 const app = express()
 const PORT = process.env.PORT || 3001
 
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 // Serve up static assets
 app.use(express.static("client/build"))
 // Add routes, both API and view
-// app.use(routes)
+app.use(routes)
 
 // Set up promises with mongoose
 mongoose.Promise = global.Promise
