3cd2303df115b4a9a6b6dc9ba0eac3d37d5e2d1d
ohsnaplol <emyb@bellsouth.net>
2018-03-29 20:25:31 -0400

    added extra password field for password verification

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -9,6 +9,8 @@ class Login extends Component {
           <input type="email"></input>
           <p>Password</p>
           <input type="password"></input>
+          <p>Verify Password</p>
+          <input type="password"></input>
           <button>Create Account</button>
         </form>
       </div>
