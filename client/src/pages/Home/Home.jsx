137266d833c78451c0187d7d7ac90646b393ac51
ohsnaplol <emyb@bellsouth.net>
2018-03-31 13:37:36 -0400

    added a little home page

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome Home</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
