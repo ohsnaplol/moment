03a6f1de07c7c955ad924144204afff7bfd34266
ohsnaplol <emyb@bellsouth.net>
2018-03-31 13:27:25 -0400

    before saving, hash password

--- models/user.js
+++ models/user.js
@@ -28,6 +28,17 @@ userSchema.methods = {
   }
 }
 
+userSchema.pre('save', function (next) {
+  if (!this.password) {
+    console.log('models/user.js =======NO PASSWORD PROVIDED=======')
+    next()
+  } else {
+    console.log('models/user.js hashPassword in pre save');
+    this.password = this.hashPassword(this.password)
+    next()
+  }
+})
+
 const User = mongoose.model("User", userSchema)
 
 module.exports = User
