5f692b59c519a12a3785cc7468f040c98c1de774
Keissy <keissy@lifecellskincare.com>
2018-04-05 12:33:17 -0400

    adding Signup.jsx file

---
+++ client/src/pages/Signup/Signup.jsx
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom'
+import API from "../../utils/API";
+
+document.body.style.backgroundcolor = '#141320'; 
+
+ const formStyle = { 
+  width: '166px', 
+  position: 'absolute', 
+  left: '565px', 
+  top: '171px', 
+  textAlign: 'center', 
+  background: 'lavenderblush'
+  paddingLeft: '177px', 
+  paddingRight: '177px', 
+  paddingBottom: '56px', 
+  paddingTop: '56px', 
+  fontFamily:  'Helvetica', 
+  borderRadius: '50px', 
+  fontWeight: 'bold', 
+} 
+
+const buttonStyle = { 
+      margin-top: '13px', 
+ }
+
+class Signup extends Component {
+  constructor() {
+    super()
+    this.state = {
+      email: "",
+      password1: "",
+      password2: ""
+    }
+    // this.handleInputChange = this.handleInputChange.bind(this)
+    // this.handleFormSubmit = this.handleFormSubmit.bind(this)
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault()
+    let password = this.state.password1
+    let email = this.state.email
+    // verify both passwords are the same
+    if (this.state.password1 === this.state.password2 && this.state.password1.length > 7) {
+      // check if email doesnt already exist in db
+      API.createUser({ 
+        email: email, 
+        password: password 
+      }).then(res => {
+        console.log('api create user res is ' + JSON.stringify(res))
+        if(res.data.error) {
+          console.log('entry exists')
+        } else {
+          this.setState({ //redirect to login page (try to)
+            redirectTo: '/'
+          })
+        }
+      }).catch(error => console.log(error))
+    } else {
+      if(this.state.password1.length < 8 )
+        console.log("Password must be at least 8 characterse")
+      if (this.state.password1 !== this.state.password2)
+        console.log("Passwords do not match")
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+        <form style={formStyle} onSubmit={this.handleFormSubmit}>
+          <label>
+            Email:
+            <input 
+              type="email" 
+              value={this.state.email} 
+              name="email"
+              onChange={this.handleInputChange}
+            ></input>
+          </label>
+          <label>
+            Password:
+            <input 
+              type="password" 
+              value={this.state.password1} 
+              name="password1"
+              onChange={this.handleInputChange}
+            ></input>
+          </label>
+          <label>
+            Verify Password:
+            <input 
+              type="password" 
+              value={this.state.password2} 
+              name="password2"
+              onChange={this.handleInputChange}
+            ></input>
+          </label>
+          <input style={buttonStyle} type="submit" value="Create Account" />
+          <Link to="/">
+            <button style={buttonStyle} >Back to Login</button>
+          </Link>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Signup;
