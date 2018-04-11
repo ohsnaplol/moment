d11a336a49592e4270d540a554195a0b90d6a670
ohsnaplol <emyb@bellsouth.net>
2018-04-11 00:53:12 -0400

    fixed an issue where you had to refresh after logging in to be logged in

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -28,7 +28,7 @@ class Login extends Component {
         // update App.js state
         this.props.updateUser({
           loggedIn: true,
-          username: response.data.username
+          id: response.data.username
         })
         // update the state to redirect to home
         this.setState({
