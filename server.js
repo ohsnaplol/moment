e72fa6710edc00db763c3b7308999f7f6aa2e398
ohsnaplol <emyb@bellsouth.net>
2018-04-18 00:50:59 -0400

    initial commit

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const morgan = require("morgan")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const routes = require("./routes")
+const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3001
+require("dotenv").config()
 // Serve up static assets
-app.use(express.static("client/build"))
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(morgan('dev'))
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -19,7 +21,7 @@ app.use(bodyParser.json())
 // Connect to the Mongo DB
 app.use(
   session({
-  secret: 'pxoqgcgoewrs', // random string to make the hash that is generated secure
+  secret: process.env.SECRET || 'pxoqgcgoewrs', // random string to make the hash that is generated secure
   store: new MongoStore({ mongooseConnection: dbConnection }),
   resave: false, //required
   saveUninitialized: false //required
@@ -32,6 +34,9 @@ app.use(passport.session()) // calls serializeUser and deserializeUser
 // Add routes, both API and view
 app.use(routes)
 
+app.get("*", (req, res) => {  
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
