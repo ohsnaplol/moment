2e524dc3441d4af88e00e262ec28acd4c477fbe2
ohsnaplol <emyb@bellsouth.net>
2018-04-19 00:08:27 -0400

    autocomplete no longer hides behind other ui elements

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -49,6 +49,17 @@ class Search extends Component {
         getItemValue={results => results.email}
         onChange={this.handleInputChange}
         items= {this.state.results}
+        menuStyle={{
+          borderRadius: '3px',
+          boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
+          background: 'rgba(255, 255, 255)',
+          padding: '2px 0',
+          fontSize: '90%',
+          position: 'fixed',
+          overflow: 'auto',
+          zIndex: '10',
+          maxHeight: '50%', // TODO: don't cheat, let it flow to the bottom
+        }}
       />
     )
   }
