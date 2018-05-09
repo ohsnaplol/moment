1b0d8c3a1a95f731023dd778b9f122b33de4d32a
ohsnaplol <emyb@bellsouth.net>
2018-05-09 14:48:02 -0400

    removed comments, made ice cream white

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -64,42 +64,19 @@ class Login extends Component {
       <div className="All-Content">
         <div className="container-div">
         <header className="header-class-login">  
-            {/* version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	  width="106.167px" height="117.195px" viewBox="0 0 106.167 117.195" enable-background="new 0 0 106.167 117.195"
-    xml:space="preserve"> */}
             <svg className="honey-login">
-            {/* <g display="none">
-	          <g display="inline">
-		
-			        <ellipse transform="matrix(0.9579 0.287 -0.287 0.9579 20.4702 -14.8719)" fill="#F1F7EE" cx="60.957" cy="62.379" rx="41.173" ry="36.28"/>
-		
-			        <ellipse transform="matrix(0.9579 0.287 -0.287 0.9579 29.1954 -8.6818)" fill="#F1F7EE" cx="44.208" cy="95.235" rx="17.28" ry="9.109"/>
-		
-			        <ellipse transform="matrix(0.9579 0.287 -0.287 0.9579 30.6884 -15.794)" fill="#F1F7EE" cx="69.211" cy="96.768" rx="17.28" ry="9.109"/>
-		
-			        <ellipse transform="matrix(0.9579 0.287 -0.287 0.9579 30.4132 -21.4538)" fill="#F1F7EE" cx="88.378" cy="93.002" rx="17.28" ry="9.109"/>
-		
-			        <ellipse transform="matrix(0.9579 0.287 -0.287 0.9579 23.9117 -3.6079)" fill="#F1F7EE" cx="24.261" cy="79.749" rx="17.28" ry="9.109"/>
-	          </g>
-	            <path display="inline" fill="#F5DEB3" d="M81.232,19.921c0,0,5.81-15.667,13.941-19.539c0,0,3.485,1.549,2.324,2.711
-		          c0,0-0.129,1.678-1.421,1.937l-1.032-2.065c0,0-5.423-0.939-11.876,16.957H81.232z"/>
-              <circle display="inline" fill="#C41617" cx="79.347" cy="25.66" r="10.136"/>
-	            <ellipse display="inline" fill="#010202" cx="48.39" cy="57.658" rx="1.291" ry="1.699"/>
-	            <ellipse display="inline" fill="#010202" cx="76.015" cy="64.078" rx="1.291" ry="1.699"/>
-	            <path display="inline" fill="#010202" d="M47.955,69.892l25.932,4.975C73.887,74.867,59.633,85.595,47.955,69.892z"/>
-            </g> */}
             <g>
 	          <g>
 		
-              <ellipse className="honey-head" transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="#ccff66" cx="54.196" cy="69.903" rx="43.351" ry="38.201"/>
+              <ellipse className="honey-head" transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="white" cx="54.196" cy="69.903" rx="43.351" ry="38.201"/>
 		
-              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="#ccff66" cx="41.894" cy="106.733" rx="18.195" ry="9.59"/>
+              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="white" cx="41.894" cy="106.733" rx="18.195" ry="9.59"/>
 		
-              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="#ccff66" cx="68.167" cy="104.422" rx="18.195" ry="9.591"/>
+              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="white" cx="68.167" cy="104.422" rx="18.195" ry="9.591"/>
 		
-              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="#ccff66" cx="87.537" cy="97.503" rx="18.194" ry="9.592"/>
+              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="white" cx="87.537" cy="97.503" rx="18.194" ry="9.592"/>
 		
-              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="#ccff66" cx="18.702" cy="93.726" rx="18.195" ry="9.592"/>
+              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="white" cx="18.702" cy="93.726" rx="18.195" ry="9.592"/>
 	          </g>
               <path className="honey-stem" transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="#F5DEB3" d="M68.673,22.524c0,0,3.6-17.221,11.463-22.524c0,0,3.871,1.068,2.842,2.46c0,0,0.129,1.768-1.176,2.238
 		          l-1.398-1.989c0,0-5.793-0.131-9.715,19.514L68.673,22.524z"/>
@@ -113,10 +90,7 @@ class Login extends Component {
         </svg>               
           <h1 className="solid-moment">
             Scoop
-          </h1>        
-          <h2 className="moment-faded">
-            Scoop
-          </h2> 
+          </h1>
         </header> 
         <form className="login-form" onSubmit={this.handleFormSubmit}>
           <div className="email-input">
