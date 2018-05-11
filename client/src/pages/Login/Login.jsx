9c830f2be35515563b610fcce335447ecd4fbe7a
ohsnaplol <emyb@bellsouth.net>
2018-05-11 15:31:22 -0400

    login page now uses bootstrap

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -3,6 +3,7 @@ import { withAlert } from 'react-alert'
 import { Link, Redirect } from 'react-router-dom'
 import API from "../../utils/API";
 import "./style.css";
+import Logo from "../../components/Logo"
 // This will be uncommented out, with the image tag, one we have an image with transparency around it. 
 // import FLOWPINK from './flowtrans.png';
 
@@ -61,65 +62,31 @@ class Login extends Component {
       return <Redirect to={{ pathname: this.state.redirectTo }} />
     } else {
     return (
-      <div className="All-Content">
-        <div className="container-div">
-        <header className="header-class-login">  
-            <svg className="honey-login">
-            <g>
-	          <g>
-		
-              <ellipse className="honey-head" transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="white" cx="54.196" cy="69.903" rx="43.351" ry="38.201"/>
-		
-              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="white" cx="41.894" cy="106.733" rx="18.195" ry="9.59"/>
-		
-              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="white" cx="68.167" cy="104.422" rx="18.195" ry="9.591"/>
-		
-              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="white" cx="87.537" cy="97.503" rx="18.194" ry="9.592"/>
-		
-              <ellipse transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="white" cx="18.702" cy="93.726" rx="18.195" ry="9.592"/>
-	          </g>
-              <path className="honey-stem" transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="#F5DEB3" d="M68.673,22.524c0,0,3.6-17.221,11.463-22.524c0,0,3.871,1.068,2.842,2.46c0,0,0.129,1.768-1.176,2.238
-		          l-1.398-1.989c0,0-5.793-0.131-9.715,19.514L68.673,22.524z"/>
-              <circle className="honey-cherry" transform="matrix(0.9899 0.1416 -0.1416 0.9899 210 20)" fill="#C41617" cx="67.606" cy="28.795" r="10.673"/>
-    	
-		          <ellipse className="honey-leftEye" transform="matrix(0.989 -0.148 0.148 0.989 192 28)" fill="#010202" cx="40.374" cy="66.952" rx="1.359" ry="1.789"/>
-	
-		          <ellipse className="honey-rightEye" transform="matrix(0.9889 -0.1484 0.1484 0.9889 192 30)" fill="#010202" cx="70.142" cy="69.318" rx="1.359" ry="1.789"/>
-              <path className="honey-mouth" transform="matrix(0.9899 0.1416 -0.1416 0.9899 213 20)" fill="#010202" d="M41.833,79.758l27.78,1.128C69.612,80.886,56.448,94.285,41.833,79.758z"/>
-          </g>
-        </svg>               
-          <h1 className="solid-moment">
-            Scoop
-          </h1>
-        </header> 
-        <form className="login-form" onSubmit={this.handleFormSubmit}>
-          <div className="email-input">
-          <p>Welcome to Scoop. Your online self in one place.</p>
-          <p>Please Login or Sign up</p>
-            <label>
-              <input placeholder= "Email" value={this.state.email} onChange={this.handleInputChange} name="email" type="email" autoComplete="current-email"/>
-            </label>
-          </div> 
-          <div className="Password-Entry">
-            <label>
-              <input placeholder="Password" value={this.state.password} onChange={this.handleInputChange} name="password" type="password" autoComplete="current-password"/>
-            </label>
+      <div className="container">
+        <div className="row">
+          <header className="col-md-6">
+            <div className="scoop-headline">
+              <Logo />
+            </div>
+          </header> 
+          <div className="card col-md-6 login-card">
+            <form onSubmit={this.handleFormSubmit} className="card-body">
+              <p>Welcome to Scoop. Your online self in one place.</p>
+              <p>Please Login or Sign up</p>
+              <div className="form-group">
+                <input className="form-control" placeholder= "Email" value={this.state.email} onChange={this.handleInputChange} name="email" type="email" autoComplete="current-email"/>
+              </div> 
+              <div className="form-group">
+                <input className="form-control" placeholder="Password" value={this.state.password} onChange={this.handleInputChange} name="password" type="password" autoComplete="current-password"/>
+              </div>
+              <button className="btn btn-dark login-btn" type="submit">Log In</button>
+              <br/>
+              <Link to="/signup">
+                <button className="btn btn-secondary">Create Account</button>
+              </Link>
+            </form>
           </div>
-          {/* trying to create padding between pw & lgin bttn */}
-          <div className="login-button-group">
-          <p></p>
-          </div>
-          <div>
-          <input className="login-button" type="submit" value="Log In"/>
-          </div>
-          <div>
-            {/* switched to link vs bttn due to standard practice */}
-          <Link to="/signup">
-            <p className="create-link">Create Account</p>
-          </Link>
-          </div>
-        </form>
-      </div>
+        </div>
       </div>
     )
   }
