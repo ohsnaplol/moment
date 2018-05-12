f6ee1cfda43adefa83447bb3cf7e9f44f8d8cb05
ohsnaplol <emyb@bellsouth.net>
2018-05-12 01:44:06 -0400

    navbar now stays on top of screen

--- client/src/components/NavBar/Navbar.jsx
+++ client/src/components/NavBar/Navbar.jsx
@@ -37,7 +37,7 @@ class Navbar extends Component {
       return <Redirect to={{ pathname: this.state.redirectTo }} />
     } else {
       return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <nav className="navbar sticky-top navbar-expand-sm navbar-light bg-light">
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
