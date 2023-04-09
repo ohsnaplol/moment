8f6a202c9b1dd21d95587b4ee68ac4e8acf8d1e7
Marcello Galvan <emyb@bellsouth.net>
2023-04-09 10:40:26 -0400

    use new mongodb addr+port

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise
 
 //your local database url
 //27017 is the default mongoDB port
-const uri = process.env.MONGODB_URI || "mongodb://localhost/moment"
+const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/moment"
 
 mongoose.connect(uri).then(
     () => { 
