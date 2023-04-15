bec1809280957b8dae0afac011a3946c71472cc3
Marcello Galvan <emyb@bellsouth.net>
2023-04-15 09:01:21 -0400

    remove deprecation warning

--- db/index.js
+++ db/index.js
@@ -1,12 +1,11 @@
 //Connect to Mongo database
 const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
 
 //your local database url
 //27017 is the default mongoDB port
 const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/moment"
 
-mongoose.connect(uri).then(
+mongoose.connect(uri, { useNewUrlParser: true }).then(
     () => { 
         /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ 
         console.log('Connected to Mongo');
